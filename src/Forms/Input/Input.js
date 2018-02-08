3233347d4b1207440b2ff11083708aa79cf485e2
Jakub Fides <jakub.fides@brandembassy.com>
2018-02-08 16:07:07 +0100

    notif 2

--- src/Forms/Input/Input.js
+++ src/Forms/Input/Input.js
@@ -19,7 +19,7 @@ export default class Input extends Component {
           {this.props.label}                
         </div>
         <div className={styles.Field}>
-          <input type="email" name={this.props.name} placeholder={this.props.placeholder} disabled={this.props.isDisabled} />
+          <input type="text" name={this.props.name} placeholder={this.props.placeholder} disabled={this.props.isDisabled} />
         </div>
         <div className={styles.Desc}>
           {this.props.desc}                
