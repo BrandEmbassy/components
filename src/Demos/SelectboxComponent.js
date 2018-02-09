b1baf75cd4fc5a175f794face8a8c756af31a8c9
Jakub Fides <jakub.fides@brandembassy.com>
2018-02-09 13:23:38 +0100

    select edited

--- src/Demos/SelectboxComponent.js
+++ src/Demos/SelectboxComponent.js
@@ -17,10 +17,10 @@ export default class InputComponent extends Component {
         </div>
         <div className="row">
           <div className="col-xs-6">
-            <Selectbox/>
+            <Selectbox label="Label here" desc="description here"/>
           </div>
         </div>
-        <div className="row padding-20">
+        <div className="row">
           <div className="col-xs-12 ">
             <h3 className="h3">html</h3>
           </div>
@@ -28,24 +28,58 @@ export default class InputComponent extends Component {
         <div className="row">
           <div className="col-xs-12 ">
             <SyntaxHighlighter language='jsx' style={base16AteliersulphurpoolLight}>
-              {beautify.html(renderToString(<Selectbox/>))}
+              {beautify.html(renderToString(<Selectbox label="Label here" desc="description here"/>))}
             </SyntaxHighlighter>
           </div>
         </div> 
-        <div className="row padding-20">
+        <div className="row">
           <div className="col-xs-12 ">
             <h3 className="h3">Wide</h3>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-6">
-            <Selectbox Wide/>
+            <Selectbox Wide label="Label here" desc="description here"/>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-12 ">
             <SyntaxHighlighter language='jsx' style={base16AteliersulphurpoolLight}>
-              {beautify.html(renderToString(<Selectbox Wide/>))}
+              {beautify.html(renderToString(<Selectbox Wide label="Label here" desc="description here"/>))}
+            </SyntaxHighlighter>
+          </div>
+        </div> 
+        <div className="row">
+          <div className="col-xs-12 ">
+            <h3 className="h3">Disabled</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-6">
+            <Selectbox Wide isDisabled label="Label here" desc="description here"/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 ">
+            <SyntaxHighlighter language='jsx' style={base16AteliersulphurpoolLight}>
+              {beautify.html(renderToString(<Selectbox isDisabled Wide label="Label here" desc="description here"/>))}
+            </SyntaxHighlighter>
+          </div>
+        </div> 
+        <div className="row">
+          <div className="col-xs-12 ">
+            <h3 className="h3">Error</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-6">
+            <Selectbox Wide isError label="Label here" desc="description here"/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 ">
+            <SyntaxHighlighter language='jsx' style={base16AteliersulphurpoolLight}>
+              {beautify.html(renderToString(<Selectbox isError Wide label="Label here" desc="description here"/>))}
             </SyntaxHighlighter>
           </div>
         </div> 
