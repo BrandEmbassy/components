1d95581d3b9cb13c4fb2e56382e5409f2b9bd26d
Jindrich Samec <jindrich.samec@xjs.cz>
2018-04-20 15:16:07 +0200

    #20787 Plugin gallery style

--- stories/livechat.stories.js
+++ stories/livechat.stories.js
@@ -9,44 +9,37 @@ import Image from '../src/node_modules/components/ChatBot/Plugin/Element/Image'
 import Text from '../src/node_modules/components/ChatBot/Plugin/Element/Text'
 import Plugin from '../src/node_modules/components/ChatBot/Plugin'
 
-// id(string, required)
-// ++type(string, required) - Type of the element.Available values:
-// ++text(string, optional)
-// ++postback(string, optional)
-// ++url(string, optional)
-// ++plugins(array[PluginElement], optional)
-
-// 'TEXT', 'TITLE', 'SUBTITLE', 'IMAGE', 'BUTTON', 'MENU', 'TEXT_AND_BUTTONS', 'QUICK_REPLIES'
-
 const repliesButtons = [
   { id: 'Nkm0hRAiE', type: 'BUTTON', text: 'Button 1', postback: 'click-on-button-1' },
   { id: 'TkGJ6CAiN', type: 'BUTTON', text: 'Button 2', postback: 'click-on-button-2' },
   { id: 'EyCyTRCi4', type: 'BUTTON', text: 'Button 3', postback: 'click-on-button-3' }
 ]
 
-const replies = [
+const quickReplies = [
   {
     id: 'Ukm0hRAiA',
-    type: 'QUICK_REPLIES',
+    type: 'QUICK_REPLY',
     elements: repliesButtons
   }
 ]
 
-const menuElements = [
-  { id: 'Ek4tPy1h4', type: 'MENU', elements: [
-    { id: 'Ek4tPy1h4', type: 'IMAGE', url: 'http://via.placeholder.com/350x150', postback: 'click-on-button-3' },
-    { id: 'Ek4tPy1h4', type: 'HEADING', text: 'Heading', postback: 'click-on-button-3' },
-    { id: 'Ek4tPy1h4', type: 'TEXT', text: 'Lorem Impsum...', postback: 'click-on-button-3' },
+const menuElement = {
+  id: 'Ek4tPy1h4',
+  type: 'MENU',
+  elements: [
+    { id: 'Ek4tPy1h2', type: 'IMAGE', url: 'https://picsum.photos/300/150', postback: 'click-on-button-3' },
+    { id: 'Ek4tPy1h3', type: 'HEADING', text: 'Heading', postback: 'click-on-button-3' },
+    { id: 'Ek4tPy1h1', type: 'TEXT', text: 'Lorem Impsum...', postback: 'click-on-button-3' },
     { id: 'Nkm0hRAiE', type: 'BUTTON', text: 'Click me!', postback: 'click-on-button-1' },
     { id: 'NkGJ6CAiN', type: 'BUTTON', text: 'No click me!', postback: 'click-on-button-2' },
     { id: 'EyCyTRCi4', type: 'BUTTON', text: 'Aww don`t click on me', postback: 'click-on-button-3' }
-  ]}
-]
+  ]
+}
 
 const galleryElements = [
-  { id: 'Ek4tPy1h4', type: 'MENU', elements: menuElements },
-  { id: '41eajy134', type: 'MENU', elements: menuElements },
-  { id: 'NkO6s1yh4', type: 'MENU', elements: menuElements }
+  menuElement,
+  menuElement,
+  menuElement,
 ]
 
 storiesOf('Chatbots elements', module)
@@ -59,7 +52,7 @@ storiesOf('Chatbots elements', module)
 
 storiesOf('Chatbots Plugins', module)
   .addDecorator(withKnobs)
-  .add('Menu', () => <Plugin elements={object('elements', menuElements)} onClick={() => { }} />)
+  .add('Menu', () => <Plugin elements={object('elements', [menuElement])} onClick={() => { }} />)
   .add('Gallery', () => <Plugin elements={object('elements', galleryElements)} onClick={() => {}} />)
-  .add('Text and buttons', () => <Plugin elements={object('elements', menuElements)} onClick={() => {}} />)
-  .add('Quick Replies', () => <Plugin elements={object('elements', replies)} onClick={() => {}} />)
+  .add('Text and buttons', () => <Plugin elements={object('elements', [menuElement])} onClick={() => {}} />)
+  .add('Quick Replies', () => <Plugin elements={object('elements', quickReplies)} onClick={() => {}} />)
