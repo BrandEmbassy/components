546c89c3eeb08caa745a8f980a568e9d29f942ed
Jindrich Samec <jindrich.samec@xjs.cz>
2018-05-16 11:29:10 +0200

    #21345 updated snapshots for gallery and carusel

--- src/package/components/ChatBot/Plugin/Carusel.test.js
+++ src/package/components/ChatBot/Plugin/Carusel.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Carusel from './Carusel'
+import elements from './__fixtures__/index.json'
 
 describe('Carusel', () => {
   it('should render carusel', () => {
-    const wrapper = render(<Carusel elements={[]} onClick={jest.fn()} hideButtons={false} />)
+    const wrapper = render(<Carusel elements={elements} onClick={jest.fn()} hideButtons={false} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
