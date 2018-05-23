6526fc01d13488254bd9a9a037bfd1d2fb2313a0
Jindrich Samec <jindrich.samec@xjs.cz>
2018-05-23 12:52:46 +0200

    #21345 removed memoized handler from quick replies

--- src/package/components/ChatBot/Plugin/Element/QuickReplies/index.jsx
+++ src/package/components/ChatBot/Plugin/Element/QuickReplies/index.jsx
@@ -1,41 +1,43 @@
-import React, {PureComponent} from 'react'
-import memoize from 'lodash.memoize'
+// @flow
+import * as React from 'react'
 import ElementType from '../ElementType'
 import Bubble from '../Text/Bubble'
 import styles from './index.css'
 
-export default class QuickReplies extends PureComponent {
-  getTextElement () {
+type Props = {
+  elements: Array<Object>,
+  onClick: Function,
+  hideButtons: boolean
+}
+export default class QuickReplies extends React.PureComponent<Props> {
+  getTextElement (): ?Object {
     const { elements } = this.props
     const textElements = elements.filter((el) => el.type === ElementType.TEXT)
     return textElements[0] ? textElements[0] : null
   }
 
-  getButtons () {
+  getButtons (): Array<Object> {
     const { elements } = this.props
     return elements.filter((el) => el.type === ElementType.BUTTON)
   }
 
-  createOnClickHandler = (postback, text) => () => {
+  createOnClickHandler = (postback: string, text: string) => (): ?Function => {
     const { onClick } = this.props
-    if (!onClick) {
-      return null
+    if (onClick) {
+      onClick(postback, text)
     }
-    return onClick(postback, text)
   }
 
-  memoizedCreateOnClickHandler = memoize(this.createOnClickHandler)
-
-  renderButtons () {
+  renderButtons (): React.Node {
     return (
       <div className={styles.ButtonsGroup}>
-        {this.getButtons().map(({ text, id, postback }) => {
+        {this.getButtons().map(({ id, postback, text }) => {
           return (
             <button
               key={id}
               className={styles.QuickReplyButton}
               title={text}
-              onClick={this.memoizedCreateOnClickHandler(postback, text)}>
+              onClick={this.createOnClickHandler(postback, text)}>
               {text}
             </button>
           )
@@ -44,7 +46,7 @@ export default class QuickReplies extends PureComponent {
     )
   }
 
-  renderMessage (textElement) {
+  renderMessage (textElement: Object): React.Node {
     return <div className={styles.Message}><Bubble text={textElement.text} /></div>
   }
 
