c23cd4d9e6d9a7e5d9781a4ba0abc3947b32b6c8
Roman Nikrmajer <roman.nikrmajer@brandembassy.com>
2018-08-24 14:48:10 +0200

    #23537 - fix of passing attributes

--- src/package/components/ChatBot/Plugin/Element/ComposedElement/index.jsx
+++ src/package/components/ChatBot/Plugin/Element/ComposedElement/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Element from '../Element'
 import styles from './index.css'
 
-export default function ComposedElement ({ elements, onClick, hideButtons, showTextAsMessage, topLevel }) {
+export default function ComposedElement ({ elements, ...composedElementRestProps }) {
   return (
     <div className={styles.ComposedElement}>
       {elements.map(({ id, ...rest }) => (
         <Element
           key={id}
           id={id}
+          {...composedElementRestProps}
           {...rest}
-          topLevel
         />
       )
       )}
