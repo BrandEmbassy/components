44a5934d3f26b94458b25dbaeb73fc4b36115edf
Jakub Fides <Jakub@MacBook-Pro-Merlog.local>
2018-09-11 15:12:41 +0200

    Double css folder fixed

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Checkbox from './package/components/Checkbox'
 import Input from './package/components/Input'
 import Label from './package/components/Label'
 import Link from './package/components/Link'
+import Linklist from './package/components/Linklist'
 import Notification from './package/components/Notification'
 import P from './package/components/P'
 import ProfilePicture from './package/components/ProfilePicture'
@@ -205,19 +206,24 @@ export default class App extends Component {
               </div>
               <div className='row'>
                 <div className='col-xs-12 padding-15'>
-                  <Link Icon='be-icon-plus' Green text='Add' />
-                  <Link Icon='be-icon-pencil' Blue text='Edit' />
-                  <Link Icon='be-icon-trash' text='Remove' />
-                  <Link Icon='be-icon-mail' text='' />
-                  <Link Icon='be-icon-check' Green text='' />
-                  <Link Icon='be-icon-more' text='' />
-                  <Link Icon='be-icon-search' Red text='' />
+                  <Linklist>
+                    <Link Icon='be-icon-plus' Green text='Add' />
+                    <Link Icon='be-icon-pencil' Blue text='Edit' />
+                    <Link Icon='be-icon-trash' text='Remove' />
+                    <Link Icon='be-icon-mail' text='' />
+                    <Link Icon='be-icon-check' Green text='' />
+                    <Link Icon='be-icon-more' text='' />
+                    <Link Icon='be-icon-search' Red text='' />
+                  </Linklist>
+                  
                 </div>
                 <div className='col-xs-12 color4 padding-10'>
-                  <Link Icon='be-icon-check' Green text='' />
-                  <Link Icon='be-icon-more' Black text='' />
-                  <Link Icon='be-icon-search' Red text='search' />
-                  <Link Icon='be-icon-mail' White text='Mail' />
+                  <Linklist>
+                    <Link Icon='be-icon-check' Green text='' />
+                    <Link Icon='be-icon-more' Black text='' />
+                    <Link Icon='be-icon-search' Red text='search' />
+                    <Link Icon='be-icon-mail' White text='Mail' />
+                  </Linklist>
                 </div>
               </div>
               <div className='row padding-10'>
