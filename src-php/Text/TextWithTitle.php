0bc9df2188dc9991f217e568a242e09b57cf40a8
Michal Brauner <michal.br@gmail.com>
2019-02-08 12:50:48 +0100

    add text with title

---
+++ src-php/Text/TextWithTitle.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Components\Text;
+
+use BrandEmbassy\Components\ArrayRenderer;
+use BrandEmbassy\Components\UiComponent;
+
+final class TextWithTitle implements UiComponent
+{
+
+    /**
+     * @var UiComponent[]|string[]
+     */
+    private $children;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @param UiComponent[]|string[]|UiComponent|string $children
+     * @param string $title
+     */
+    public function __construct($children, string $title)
+    {
+        $this->children = \is_array($children) ? $children : [$children];
+        $this->title = $title;
+    }
+
+    public function render(): string
+    {
+        return '<span title="' . $this->title . '">' . ArrayRenderer::render($this->children) . '</span>';
+    }
+
+}
