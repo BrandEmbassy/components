02142fb9544853d3800c1d16249cd139ab45a28c
Petr Hejna <hejna.petr@protonmail.com>
2019-03-10 14:11:05 +0100

    Add title for ProfilePicture image + addes snpshot test for ProfileImage component

---
+++ src-php/ProfilePicture/ProfilePictureTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Components\ProfilePicture;
+
+use BrandEmbassy\Components\SnapshotAssertTrait;
+use GuzzleHttp\Psr7\Uri;
+use PHPUnit\Framework\TestCase;
+
+final class ProfilePictureTest extends TestCase
+{
+
+    use SnapshotAssertTrait;
+
+    /**
+     * @dataProvider profilePicturesProvider
+     * @param string $expectedSnapshot
+     * @param ProfilePicture $picture
+     */
+    public function testRendering(string $expectedSnapshot, ProfilePicture $picture): void
+    {
+        $this->assertSnapshot($expectedSnapshot, $picture);
+    }
+
+    public function profilePicturesProvider(): array
+    {
+        $size24 = ProfilePictureSize::get(ProfilePictureSize::SIZE_24);
+        $dummyUri = new Uri('https://example.com/a.png');
+
+        return [
+            [__DIR__ . '/__snapshots__/pictureNoTitle.html', new ProfilePicture($dummyUri, $size24)],
+            [
+                __DIR__ . '/__snapshots__/pictureWithTitle.html',
+                new ProfilePicture($dummyUri, $size24, 'Lorem Ipsum title'),
+            ],
+        ];
+    }
+}
