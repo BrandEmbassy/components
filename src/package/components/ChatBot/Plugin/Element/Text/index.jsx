0e998816cb9be4e834f3f52abe01664ef7b336de
Jakub Fides <jakub.fides@gmail.com>
2019-03-20 16:02:25 +0100

    Markdown mimeType

--- src/package/components/ChatBot/Plugin/Element/Text/index.jsx
+++ src/package/components/ChatBot/Plugin/Element/Text/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './index.css'
+import Markdown from './../Markdown'
+import TextType from '../../../TextType'
 
 export type TextElementProps = {
   content: string,
-  showAsMessage?: boolean
+  showAsMessage?: boolean,
+  mimeType?: string
 }
 
-export default function Text ({ content, showAsMessage = false }) {
+export default function Text ({ content, showAsMessage = false, mimeType = TextType.TEXT_PLAIN }) {
   const className = showAsMessage ? styles.MessageText : styles.Text
+  if (mimeType === TextType.TEXT_MARKDOWN) {
+    return (
+      <Markdown text={content} />
+    )
+  }
   return <p className={className}>{content}</p>
 }
---
+++ src/package/components/ChatBot/TextType.jsx
@@ -0,0 +1,4 @@
+export default {
+  TEXT_PLAIN: 'text/plain',
+  TEXT_MARKDOWN: 'text/markdown'
+}
