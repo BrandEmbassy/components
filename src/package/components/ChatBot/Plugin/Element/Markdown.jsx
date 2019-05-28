2a1895114647c0e2d4b7f46927929190fa75fbb7
Jakub Fides <jakub.fides@gmail.com>
2019-05-28 13:53:11 +0200

    markdown image size

--- src/package/components/ChatBot/Plugin/Element/Markdown.jsx
+++ src/package/components/ChatBot/Plugin/Element/Markdown.jsx
@@ -1,13 +1,16 @@
 // @flow
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import style from './Markdown.css'
 
 type Props = {
   text: string
 }
 
-export default function Markdown (props:Props) {
+export default function Markdown (props: Props) {
   return (
-    <ReactMarkdown source={props.text} />
+    <div className={style.Markdown}>
+      <ReactMarkdown source={props.text} />
+    </div>
   )
 }
