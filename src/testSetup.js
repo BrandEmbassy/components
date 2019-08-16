95b5ffa1b21c8b4797f6aa0654a93b80471ae4da
Antonín Simerský <antonin.simersky@live.com>
2019-08-16 15:27:32 +0200

    Fix linter

--- src/testSetup.js
+++ src/testSetup.js
@@ -1,30 +1,30 @@
 /* eslint-disable no-console */
-import { JSDOM } from "jsdom";
+import { JSDOM } from 'jsdom'
 // Polyfills
-import "../config/polyfills";
-import { configure, shallow, mount, render } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import '../config/polyfills'
+import { configure, shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-global.__VERSION__ = require("../package.json").version;
+global.__VERSION__ = require('../package.json').version
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
 /// /////
 // JSDOM
-const dom = new JSDOM("<html><head></head><body></body></html>");
-const win = dom.window;
-const doc = dom.window.document;
+const dom = new JSDOM('<html><head></head><body></body></html>')
+const win = dom.window
+const doc = dom.window.document
 
 // Set our data
-win.process = { env: process.env };
+win.process = { env: process.env }
 
 // propagateProperties(win, global);
 
-global.shallow = shallow;
-global.mount = mount;
-global.render = render;
-global.document = doc;
-global.window = win;
+global.shallow = shallow
+global.mount = mount
+global.render = render
+global.document = doc
+global.window = win
 global.navigator = {
-  userAgent: "node.js"
-};
+  userAgent: 'node.js'
+}
