4cc9c021b2e3a899ab29854c1b5e4f1fae8093b9
Jan Vesely <janvesely@janvesely.net>
2019-11-01 11:34:44 +0800

    add strong html tag

---
+++ src-php/Typography/StrongTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Components\Typography;
+
+use BrandEmbassy\Components\SnapshotAssertTrait;
+use PHPUnit\Framework\TestCase;
+
+final class StrongTest extends TestCase
+{
+    use SnapshotAssertTrait;
+
+
+    /**
+     * @dataProvider strongDataProvider
+     * @param string $snapshot
+     * @param Strong $strong
+     */
+    public function testShouldRenderParagraph(string $snapshot, Strong $strong): void
+    {
+        $this->assertSnapshot($snapshot, $strong);
+    }
+
+
+    /**
+     * @return mixed[]
+     */
+    public function strongDataProvider(): array
+    {
+        return [
+            [
+                __DIR__ . '/__snapshots__/strong.html',
+                new Strong('Foo bar baz'),
+            ],
+            [
+                __DIR__ . '/__snapshots__/strongXss.html',
+                new Strong('Foo <script>alert(\'I am XSS Attack\');</script>'),
+            ],
+        ];
+    }
+}
