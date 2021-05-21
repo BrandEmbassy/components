1ab502cd634aa1e5803f335802aaf6ebfb224362
Ara <araseli.condas@niceincontact.com>
2021-05-21 17:36:43 -0600

    fix uriparser errors

--- src-php/Navigation/Pagination/UriParser.php
+++ src-php/Navigation/Pagination/UriParser.php
@@ -4,21 +4,15 @@ namespace BrandEmbassy\Components\Navigation\Pagination;
 
 use BrandEmbassy\Components\Controls\UriRenderer;
 use Psr\Http\Message\UriInterface;
+use function array_key_exists;
+use function explode;
+use function is_numeric;
+use function parse_str;
 
 final class UriParser
 {
     private const BASE_URL_INDEX = 0;
     private const FIRST_PAGE = 1;
-
-    /**
-     * @var UriInterface
-     */
-    private $uri;
-
-    /**
-     * @var string
-     */
-    private $url;
 
     /**
      * @var string
@@ -33,9 +27,8 @@ final class UriParser
 
     public function __construct(UriInterface $uri)
     {
-        $this->uri = $uri;
-        $this->url = UriRenderer::urlToString($uri);
-        $this->baseUrl = explode('?', $this->url)[self::BASE_URL_INDEX];
+        $url = UriRenderer::urlToString($uri);
+        $this->baseUrl = explode('?', $url)[self::BASE_URL_INDEX];
 
         $this->queryParams = [];
         parse_str($uri->getQuery(), $this->queryParams);
